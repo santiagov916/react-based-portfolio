b52c375a71407fb413ecb8ddecc6e3b9e03d4da1
Santiago Venegas <santiagovenegas@Santiagos-MacBook-Pro.local>
2022-03-12 21:18:48 -0800

    publish no errors

--- src/components/Cover/index.js
+++ src/components/Cover/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import coverPhoto from '../../assets/cover/0.jpg';
-
 import { Image } from 'react-bootstrap';
 import Nav from '../Nav';
 
 function Cover() {
     return(
-        <div fluid className='cover-bg'>
+        <div fluid='true' className='cover-bg'>
             <Nav></Nav>
                 <p className='mission'>The mission has always been to succeed, defy odds, and conquer mountains: one step at a time. So how do I do that with programming? Now its one bug at a time, a new language implemented, one feature, a vision adopted on screen, a business venture with the perfect website; publish! I sign my name on every piece I touch leaving it better than I found it, </p>
                 <p className='cover-text'>
