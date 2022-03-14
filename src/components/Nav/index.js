1b98e04fce890f1bf7a5336c94228267c6295f5f
Santiago Venegas <santiagovenegas@Santiagos-MacBook-Pro.local>
2022-03-14 15:56:30 -0700

    gh-pages ready

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -3,6 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Nav } from 'react-bootstrap';
 
 function Navbar() {
+
     return(
         <>
         <Nav fixed='top' className="justify-content-end" activeKey="/home">
@@ -10,17 +11,18 @@ function Navbar() {
             <Nav.Link href="/home">Mission</Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link href="/#projects">Projects</Nav.Link>
+            <Nav.Link href="#projects">Projects</Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link href="/#resume">Resume</Nav.Link>
+            <Nav.Link href="#resume">Resume</Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link href="/#contact" >
+            <Nav.Link href="#contact" >
               Contact
             </Nav.Link>
           </Nav.Item>
         </Nav>
+
       </>
     )
 };
